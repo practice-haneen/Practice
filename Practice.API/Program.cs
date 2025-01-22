3c845d4f931178e1b9a0a52890813028956f3053
Haneen Fakhouri <h.fakhouri.ext@Tahaluf.ae>
2025-01-22 15:43:23 +0300

    comprehensive  understanding of program.cs

--- Practice.API/Program.cs
+++ Practice.API/Program.cs
@@ -1,23 +1,73 @@
+/*
+ * configure the host
+ * Add services to the container.
+ */
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/*
+ * register services and dependency injection
+ */
+ConfigureServices(builder.Services);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+/*
+ * configures http request pipeline
+ */
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*
+ * define middleware pipeline 
+ */
+Configuer(app, app.Environment);
+
+
+static void ConfigureServices(IServiceCollection services)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
 }
 
-app.UseAuthorization();
+static void Configuer(IApplicationBuilder app, IWebHostEnvironment environment)
+{
+    if (environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.MapControllers();
+    // the words Use/Map/Run are called delegates
+    app.UseCors("AllowAll");
 
-app.Run();
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpionts =>
+    endpionts.MapControllers());
+
+
+    app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("Middleware1: Incoming Request\n");
+        //Calling the Next Middleware Component
+        await next();
+        await context.Response.WriteAsync("Middleware1: Outgoing Response\n");
+    });
+    //Second Middleware Component Registered using Use Extension Method
+    app.Use(async (context, next) =>
+    {
+        await context.Response.WriteAsync("Middleware2: Incoming Request\n");
+        //Calling the Next Middleware Component
+        await next();
+        await context.Response.WriteAsync("Middleware2: Outgoing Response\n");
+    });
+    //Third Middleware Component Registered using Run Extension Method
+    app.Run(async (context) =>
+    {
+        await context.Response.WriteAsync("Middleware3: Incoming Request handled and response generated\n");
+        //Terminal Middleware Component i.e. cannot call the Next Component
+    });
+
+    //app.Run();
+}
